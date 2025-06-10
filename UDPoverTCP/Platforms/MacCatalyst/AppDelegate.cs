c772a28722c3ba6af9558c743f694c0349ef4674
HapiFive <1143910315@qq.com>
2025-06-10 15:19:21 +0800

    添加项目文件。

---
+++ UDPoverTCP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UDPoverTCP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
